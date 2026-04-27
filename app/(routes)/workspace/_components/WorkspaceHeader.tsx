2d720af38b1339716259570cce2ed65306ce4a8c
ashutosh <ashutoshpro11@gmail.com>
2026-04-28 00:22:09 +0530

    Add fileName prop to WorkspaceHeader component

--- app/(routes)/workspace/_components/WorkspaceHeader.tsx
+++ app/(routes)/workspace/_components/WorkspaceHeader.tsx
@@ -3,25 +3,21 @@ import { Link, Save } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
-function WorkspaceHeader({onSave}:any) {
+function WorkspaceHeader({ onSave, fileName }: any) {
   return (
     <div className='p-3 border-b flex justify-between items-center'>
       <div className='flex gap-2 items-center'>
-        <Image src={'/logo2.png'}
-          alt='logo'
-          height={40}
-          width={40} />
-        <h2>File Name</h2>
+        <Image src={'/logo2.png'} alt='logo' height={40} width={40} />
+        <h2 className='font-medium'>{fileName || "Untitled File"}</h2>
       </div>
+
       <div className='flex items-center gap-4'>
-        <Button className='h-8 text-[14px]
-        gap-2 bg-teal-500 hover:bg-teal-600 cursor-pointer'
-        onClick={()=>onSave()}
-        > 
-        <Save className='h-4 w-4' /> Save </Button>
-        {/* <Button className='h-8 text-[12px]
-        gap-2 bg-blue-600 hover:bg-blue-700'>
-          Share <Link className='h-4 w-4' /> </Button> */}
+        <Button
+          className='h-8 text-[14px] gap-2 bg-teal-500 hover:bg-teal-600 cursor-pointer'
+          onClick={() => onSave()}
+        >
+          <Save className='h-4 w-4' /> Save
+        </Button>
       </div>
     </div>
   )
